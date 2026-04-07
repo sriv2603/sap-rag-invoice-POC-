b9b329762e1c944268933edd5b1337847027a99f
sriv2603 <srivaishnavihari95@gmail.com>
2026-04-06 19:22:26 -0500

    Add files via upload

---
+++ app/data.py
@@ -0,0 +1,22 @@
+documents = [
+    {
+        "id": 1,
+        "text": "Invoice 900001 belongs to vendor ABC Supplies. Invoice amount is 1250 USD. Payment term is NET30. Status is Posted."
+    },
+    {
+        "id": 2,
+        "text": "Invoice 900002 belongs to vendor Global Industrial. Invoice amount is 5400 USD. Payment term is NET45. Status is Parked."
+    },
+    {
+        "id": 3,
+        "text": "Invoice 900003 belongs to vendor Sunrise Tech. Invoice amount is 2300 USD. Payment term is Immediate. Status is Blocked for payment."
+    },
+    {
+        "id": 4,
+        "text": "Vendor ABC Supplies has open invoices in the system. Invoice 900001 is due in 30 days."
+    },
+    {
+        "id": 5,
+        "text": "Blocked invoices usually require finance review before payment can be released."
+    }
+]
